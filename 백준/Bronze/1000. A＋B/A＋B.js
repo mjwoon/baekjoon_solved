e3d88dc494e6e54464addc6ffdabbed9f710c113
mjwoon <jwmoon@hanyang.ac.kr>
2025-03-05 13:22:13 +0900

    [Bronze V] Title: A+B, Time: 96 ms, Memory: 9348 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+// TODO: 제출 시 경로 변환 필수 ("/dev/stdin")
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+// 풀이
+function solution(arr) {
+  const [a, b] = arr[0].split(' ');
+  return Number(a) + Number(b);
+}
+
+console.log(solution(input));
